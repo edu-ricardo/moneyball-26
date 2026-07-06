915d5155dcdcf1a6b8bd5837b60784960f07639c
eduardo.florencio <eduardo.florencio@linx.com.br>
2026-07-06 16:13:17 -0300

    Fix Express 5 wildcard routing error

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
   const clientDist = path.join(__dirname, '../../client/dist');
   app.use(express.static(clientDist));
   
-  app.get('*', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(clientDist, 'index.html'));
   });
 }
